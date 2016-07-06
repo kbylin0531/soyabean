a0ee76e48dc6846fb37ab4b19cfcc301f61736e9
linzhv <linzhv@outlook.com>
2016-07-06 11:40:43 +0800

    删除demo

--- Public/demo/codeLib.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kbylin on 21/05/16.
- */
-
-scrollTo = function (el, offeset) {
-    var pos = (el && el.size() > 0) ? el.offset().top : 0;
-    el && (pos = pos - $("header").height() + (offeset ? offeset : -1 * el.height()));
-    $('html,body').animate({scrollTop: pos}, 'slow');
-};
--- Public/demo/processbar/progress.js
+++
@@ -1,6 +0,0 @@
-/**
- * 纯js进度条
- * Created by kiner on 15/3/22.
- */
-
-
